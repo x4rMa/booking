9a66c21b00643fdbec0246d4a985b4c87e0a20a6
Benjamin Borbe <bborbe@rocketnews.de>
2015-08-01 17:01:54 +0200

    datetime custom validation

--- files/js/directives.js
+++ files/js/directives.js
@@ -2,3 +2,22 @@
 
 angular.module('bookingDirectives', []);
 
+angular.module('bookingDirectives').directive('datetime', ['$http', function ($http) {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      ctrl.$parsers.unshift(function(viewValue) {
+        var INTEGER_REGEXP = /^\d\d\d\d-\d\d-\d\d[\sT]\d\d:\d\d(:\d\d)$/;
+        if (INTEGER_REGEXP.test(viewValue)) {
+          // it is valid
+          ctrl.$setValidity('integer', true);
+          return viewValue;
+        } else {
+          // it is invalid, return undefined (no model update)
+          ctrl.$setValidity('integer', false);
+          return undefined;
+        }
+      });
+    }
+  }
+}]);
